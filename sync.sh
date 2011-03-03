7113510eabf58812e080d5672dcf7839f37c2e88
foxbei <foxbei@163.com>
2011-03-03 15:06:46 +0800

    date+

--- sync.sh
+++ sync.sh
@@ -1,12 +1,13 @@
-echo "Vim_conf Github sync begin ...... "
+Dirname=basename $PWD
+echo "$Dirname haGithub sync begin ...... "
 echo "Begin exec: git add ."
 git add . 
 echo "Begin exec: git commit -a -m 'commit message'"
-echo -n "Please input commit message: "
+echo -n "Please input $Dirname commit message: "
 read Msg
 Update='date'
 git commit -a -m $Update+$Msg
 echo "Begin exec: git push "
 echo "Pushing, please waiting ......"
 git push
-echo "Vim_conf Github sync finished, exit. "
+echo "$Dirname Github sync finished, exit. "
